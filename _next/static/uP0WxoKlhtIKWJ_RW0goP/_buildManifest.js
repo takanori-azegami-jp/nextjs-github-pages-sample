1ddb5a91558a041ddd0ac70524456b2eba61677b
takanori-azegami-jp <takanori-azegami-jp@users.noreply.github.com>
2023-07-10 03:33:38 +0000

    deploy: 2330286338eb14d4a84274de971e7920c70de3d2

--- _next/static/uP0WxoKlhtIKWJ_RW0goP/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-80b30ecf4912224f.js"],"/_error":["static/chunks/pages/_error-3986dd5834f581dc.js"],"/linkSample":["static/chunks/pages/linkSample-c68f8ab933e939ab.js"],sortedPages:["/","/_app","/_error","/linkSample"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
